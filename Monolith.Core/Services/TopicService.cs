2ff7fbd813fc11b7d5b3eaa6043cb38c039c72ea
Ciaran <ciaranselwyn@yahoo.co.nz>
2019-12-08 17:54:41 +0000

    no message

--- Monolith.Core/Services/TopicService.cs
+++ Monolith.Core/Services/TopicService.cs
@@ -5,6 +5,7 @@ using Monolith.Core.Repositories;
 using Monolith.Domain.BusinessObjects;
 using Monolith.Domain.Context;
 using Monolith.Domain.Interfaces;
+using Monolith.Domain.Mappers;
 using Monolith.Domain.Models.Topic;
 
 namespace Monolith.Core.Services
@@ -13,11 +14,32 @@ namespace Monolith.Core.Services
     {
         private readonly PrimarydbContext _context;
         private readonly ITopicRepository _topicRepository;
+        private readonly ICourseRepository _courseRepository;
         
-        public TopicService(PrimarydbContext context)
+        public TopicService(PrimarydbContext context, ITopicRepository topicRepository)
         {
             _context = context;
-            _topicRepository = new TopicRepository(context);
+            _topicRepository = topicRepository;
+            _courseRepository = new CourseRepository(_context);
+        }
+        
+        public IEnumerable<TopicDto> GetTopicsForCourse(int courseId)
+        {
+            // check the course exists
+            if (!_courseRepository.CourseExists(courseId))
+            {
+                return null;
+            }
+            
+            var topics = _topicRepository.GetTopicsForCourse(courseId);
+
+            if (topics == null)
+            {
+                return null;
+            }
+
+            // Map to topic response model
+            return topics.Select(topic => TopicToTopicDto.Map(topic)).ToList();
         }
         
         public bool CreateTopic(int courseId, CreateTopicRequest request)
